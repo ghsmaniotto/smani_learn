5106e03d53c4cbda3c2d774a07370f16314cf962
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-09-14 06:42:53 -0300

    Extract Funcionario method to reajust salary to a class
    It was done to improve the coesion of the Funcionario class.

--- good_practices/coding/src/br/com/alura/rh/model/Funcionario.java
+++ good_practices/coding/src/br/com/alura/rh/model/Funcionario.java
@@ -1,10 +1,7 @@
 package br.com.alura.rh.model;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.LocalDate;
-
-import br.com.alura.rh.ValidacaoException;
 
 public class Funcionario {
 
@@ -21,12 +18,8 @@ public class Funcionario {
 		this.salario = salario;
 	}
 
-	public void reajustarSalario(BigDecimal aumento) {
-		BigDecimal percentualReajuste = aumento.divide(salario, RoundingMode.HALF_UP);
-		if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
-		}
-		this.salario = this.salario.add(aumento);
+	public void atualizarSalario(BigDecimal novoSalario) {
+		this.salario = novoSalario;
 		this.dataUltimoReajuste = LocalDate.now();
 	}
 
---
+++ good_practices/coding/src/br/com/alura/rh/model/ReajusteService.java
@@ -0,0 +1,22 @@
+package br.com.alura.rh.model;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import br.com.alura.rh.ValidacaoException;
+
+public class ReajusteService {
+
+  public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento){
+    BigDecimal salarioAtual = funcionario.getSalario();
+
+    BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
+    if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
+      throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
+    }
+
+    BigDecimal salarioReajustado = salarioAtual.add(aumento);
+    funcionario.atualizarSalario(salarioReajustado);
+  }
+  
+}

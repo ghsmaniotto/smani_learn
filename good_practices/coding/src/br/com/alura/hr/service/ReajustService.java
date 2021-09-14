d18ae1cc8902a001bd6c41823c52c1498689625a
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-09-14 08:16:08 -0300

    Throw exception when last reajust was before 6 months

--- good_practices/coding/src/br/com/alura/hr/service/ReajustService.java
+++ good_practices/coding/src/br/com/alura/hr/service/ReajustService.java
@@ -2,22 +2,31 @@ package br.com.alura.hr.service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 import br.com.alura.hr.ExceptionValidation;
 import br.com.alura.hr.model.Employee;
 
 public class ReajustService {
 
-  public void reajustEmployeeSalary(Employee Employee, BigDecimal aumento){
-    BigDecimal currentSalary = Employee.getSalary();
+  public void reajustEmployeeSalary(Employee employee, BigDecimal aumento){
+    BigDecimal currentSalary = employee.getSalary();
 
     BigDecimal percentageOfReajust = aumento.divide(currentSalary, RoundingMode.HALF_UP);
     if (percentageOfReajust.compareTo(new BigDecimal("0.4")) > 0) {
       throw new ExceptionValidation("The reajust cant be higher than 40% of salary!");
     }
 
+    LocalDate dateOfLastReajust = employee.getDateOfLastReajust();
+    LocalDate currentDate = LocalDate.now();
+    long monthsSinceLastReajust = ChronoUnit.MONTHS.between(dateOfLastReajust, currentDate);
+    if (monthsSinceLastReajust < 6) {
+      throw new ExceptionValidation("Time between reajusts must be higher than 6 months!");
+    }
+
     BigDecimal salaryReajustado = currentSalary.add(aumento);
-    Employee.atualizarSalary(salaryReajustado);
+    employee.atualizarSalary(salaryReajustado);
   }
   
 }

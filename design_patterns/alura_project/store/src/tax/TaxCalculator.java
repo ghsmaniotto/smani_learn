3a26af66db33cda6a9c6f35f8fa043c25762769a
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-09-24 07:08:37 -0300

    Change tax calculator to use tax calculate method
    Instead of switch case we apply Strategy pattern

--- design_patterns/alura_project/store/src/tax/TaxCalculator.java
+++ design_patterns/alura_project/store/src/tax/TaxCalculator.java
@@ -6,15 +6,7 @@ import budget.Budget;
 
 public class TaxCalculator {
   
-  public BigDecimal calculate(Budget budget, TaxType taxType){
-    switch (taxType) {
-      case ICMS:
-        return budget.getValue().multiply(new BigDecimal("0.1"));
-      case ISS:
-        return budget.getValue().multiply(new BigDecimal("0.06"));
-      default:
-        return BigDecimal.ZERO;
-    }
-    
+  public BigDecimal calculate(Budget budget, Tax tax){
+    return tax.calculate(budget);
   }
 }

c195dd4c329648d3b236fbe2502da8becf62b491
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-09-24 06:49:13 -0300

    Calculate tax according to some tax type

--- design_patterns/alura_project/store/src/tax/TaxCalculator.java
+++ design_patterns/alura_project/store/src/tax/TaxCalculator.java
@@ -6,7 +6,15 @@ import budget.Budget;
 
 public class TaxCalculator {
   
-  public BigDecimal calculate(Budget budget){
-    return budget.getValue().multiply(new BigDecimal("0.1"));
+  public BigDecimal calculate(Budget budget, TaxType taxType){
+    switch (taxType) {
+      case ICMS:
+        return budget.getValue().multiply(new BigDecimal("0.1"));
+      case ISS:
+        return budget.getValue().multiply(new BigDecimal("0.06"));
+      default:
+        return BigDecimal.ZERO;
+    }
+    
   }
 }
---
+++ design_patterns/alura_project/store/src/tax/TaxType.java
@@ -0,0 +1,6 @@
+package tax;
+
+public enum TaxType {
+  ICMS,
+  ISS;
+}

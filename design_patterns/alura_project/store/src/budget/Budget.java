5dd86e9a1dfd757375d6521eebd45d02539952bd
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-09-27 08:05:52 -0300

    Add status to budget class and calculation for extra discount

--- design_patterns/alura_project/store/src/budget/Budget.java
+++ design_patterns/alura_project/store/src/budget/Budget.java
@@ -2,13 +2,34 @@ package budget;
 
 import java.math.BigDecimal;
 
+import budget.status.BudgetStatus;
+
 public class Budget {
   private BigDecimal value;
   private int itemsCount;
+  private BudgetStatus status;
 
   public Budget(BigDecimal value, int itemsCount) {
     this.value = value;
     this.itemsCount = itemsCount;
+  }
+
+  public void applyExtraDiscount() {
+    BigDecimal extraDiscountValue = this.status.calculateExtraDiscountValue(this);
+
+    this.value = this.value.subtract(extraDiscountValue);
+  }
+
+  public void approve() {
+    this.status.approve(this);
+  }
+
+  public void reprove() {
+    this.status.reprove(this);
+  }
+
+  public void complete() {
+    this.status.complete(this);
   }
 
   public BigDecimal getValue() {
@@ -17,5 +38,13 @@ public class Budget {
 
   public int getItemsCount() {
     return itemsCount;
+  }
+
+  public BudgetStatus getStatus() {
+    return status;
+  }
+
+  public void setStatus(BudgetStatus status) {
+    this.status = status;
   }
 }

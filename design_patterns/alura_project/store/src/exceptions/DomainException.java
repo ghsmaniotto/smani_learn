618042d8686ba868b26e536ba77cfdfdfdbdb691
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-09-27 08:18:51 -0300

    Add domain exception to project

---
+++ design_patterns/alura_project/store/src/exceptions/DomainException.java
@@ -0,0 +1,8 @@
+package exceptions;
+public class DomainException extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+
+  public DomainException(String message) {
+    super(message);
+  }
+}

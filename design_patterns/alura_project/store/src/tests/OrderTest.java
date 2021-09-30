054bdf34f31e4e5d8451f1150e1eb82800eeb532
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-09-30 07:33:51 -0300

    Refact order test by using order builder class

--- design_patterns/alura_project/store/src/tests/OrderTest.java
+++ design_patterns/alura_project/store/src/tests/OrderTest.java
@@ -1,22 +1,18 @@
 package tests;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
-
-import budget.Budget;
-import order.Order;
+import order.OrderBuilder;
 
 public class OrderTest {
 
   public static void main(String[] args) {
-    Budget budget = new Budget(new BigDecimal("200.0"), 6);
-    LocalDateTime date = LocalDateTime.now();
-    String customer = "Fulano da Silva e Sauro";
 
-    Order order = new Order(customer, date, budget);
+    String customer = args[0];
+    BigDecimal budgetValue = new BigDecimal(args[1]);
+    int itemsCount = Integer.parseInt(args[2]);
 
-    System.out.println("Persit order into the database");
-    System.out.println("Send email to customer warning new order");
+    OrderBuilder builder = new OrderBuilder(customer, budgetValue, itemsCount);
+    builder.execute();
   }
 
 }

7d2dd3302debee563843fd7535714c9b5aa29400
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-09-30 08:12:26 -0300

    Add class to send an order email

---
+++ design_patterns/alura_project/store/src/order/actions/SendOrderEmail.java
@@ -0,0 +1,11 @@
+package order.actions;
+
+import order.Order;
+
+public class SendOrderEmail {
+
+  public void execute(Order order) {
+    System.out.println("Send email to inform that the order" + order.toString() + " was created with success!");
+  }
+
+}

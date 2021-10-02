880a7f65d9d55b27a513660c7107a6ff91d2ad21
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-10-02 17:03:43 -0300

    Update ISS to accept others taxes

--- design_patterns/alura_project/store/src/tax/ISS.java
+++ design_patterns/alura_project/store/src/tax/ISS.java
@@ -4,8 +4,12 @@ import java.math.BigDecimal;
 
 import budget.Budget;
 
-public class ISS implements Tax {
-  public BigDecimal calculate(Budget budget){
+public class ISS extends Tax {
+  public ISS(Tax anotherTax){
+    super(anotherTax);
+  }
+
+  public BigDecimal calculateTax(Budget budget){
     return budget.getValue().multiply(new BigDecimal("0.06"));
   }
 }

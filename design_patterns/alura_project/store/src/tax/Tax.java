c4eebcc9aeda23cdc4cf9f82be5bcab392b50c29
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-10-02 17:03:26 -0300

    Allow tax class to receive other taxes to calculate

--- design_patterns/alura_project/store/src/tax/Tax.java
+++ design_patterns/alura_project/store/src/tax/Tax.java
@@ -4,6 +4,24 @@ import java.math.BigDecimal;
 
 import budget.Budget;
 
-public interface Tax {
-  public BigDecimal calculate(Budget budget);
+public abstract class Tax {
+
+  private Tax anotherTax;
+
+  public Tax(Tax anotherTax) {
+    this.anotherTax = anotherTax;
+  }
+
+  protected abstract BigDecimal calculateTax(Budget budget);
+  
+  public BigDecimal calculate(Budget budget){
+    BigDecimal currentTaxValue = this.calculateTax(budget);
+    BigDecimal anotherTaxValue = BigDecimal.ZERO;
+    
+    if(anotherTax != null){
+      anotherTaxValue = anotherTax.calculateTax(budget); 
+    }
+    
+    return currentTaxValue.add(anotherTaxValue); 
+  };
 }

561a708bd7516442c0076799c3da2b75e7652b6a
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-10-02 17:03:53 -0300

    Update ICMS to accept others taxes

--- design_patterns/alura_project/store/src/tax/ICMS.java
+++ design_patterns/alura_project/store/src/tax/ICMS.java
@@ -4,8 +4,12 @@ import java.math.BigDecimal;
 
 import budget.Budget;
 
-public class ICMS implements Tax {
-  public BigDecimal calculate(Budget budget){
+public class ICMS extends Tax {
+  public ICMS(Tax anotherTax){
+    super(anotherTax);
+  }
+
+  public BigDecimal calculateTax(Budget budget){
     return budget.getValue().multiply(new BigDecimal("0.1"));
   }
 }

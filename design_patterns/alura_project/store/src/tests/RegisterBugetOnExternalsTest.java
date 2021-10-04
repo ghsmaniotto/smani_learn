fadce10f27f93619f7d8796a8c184d27aa3b11f5
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-10-04 07:56:30 -0300

    Update register budget on external test
    Update according to new budget structure

--- design_patterns/alura_project/store/src/tests/RegisterBugetOnExternalsTest.java
+++ design_patterns/alura_project/store/src/tests/RegisterBugetOnExternalsTest.java
@@ -8,7 +8,10 @@ import http.JavaHttpClient;
 public class RegisterBugetOnExternalsTest {
   
   public static void main(String[] args){
-    Budget budget = new Budget(new BigDecimal("200.0"), 6);
+    Budget budget = new Budget();
+    budget.addItem(new budget.BudgetItem(new BigDecimal("100.0")));
+    budget.addItem(new budget.BudgetItem(new BigDecimal("100.0")));
+
     budget.approve();
     budget.complete();
 

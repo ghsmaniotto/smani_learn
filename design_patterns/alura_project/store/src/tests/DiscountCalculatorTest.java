717415b12890236f19403471338ee0b605e2f168
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-10-04 08:01:04 -0300

    Update discount calculator test according budget items

--- design_patterns/alura_project/store/src/tests/DiscountCalculatorTest.java
+++ design_patterns/alura_project/store/src/tests/DiscountCalculatorTest.java
@@ -2,16 +2,28 @@ package tests;
 import java.math.BigDecimal;
 
 import budget.Budget;
+import budget.BudgetItem;
 import discount.DiscountCalculator;
 
 public class DiscountCalculatorTest {
   
   public static void main(String[] args){
-    Budget first_budget = new Budget(new BigDecimal("200.0"), 6);
-    Budget second_budget = new Budget(new BigDecimal("1000.0"), 4);
+    Budget firstBudget = new Budget();
+    
+    firstBudget.addItem(new BudgetItem(new BigDecimal("50.0")));
+    firstBudget.addItem(new BudgetItem(new BigDecimal("50.0")));
+    firstBudget.addItem(new BudgetItem(new BigDecimal("50.0")));
+    firstBudget.addItem(new BudgetItem(new BigDecimal("50.0")));
+    firstBudget.addItem(new BudgetItem(new BigDecimal("50.0")));
+    firstBudget.addItem(new BudgetItem(new BigDecimal("50.0")));
+    
+    Budget secondBudget = new Budget();
+    secondBudget.addItem(new BudgetItem(new BigDecimal("500.0")));
+    secondBudget.addItem(new BudgetItem(new BigDecimal("500.0")));
+    
     DiscountCalculator discountCalculator = new DiscountCalculator();
 
-    System.out.println(discountCalculator.calculate(first_budget));
-    System.out.println(discountCalculator.calculate(second_budget));
+    System.out.println(discountCalculator.calculate(firstBudget));
+    System.out.println(discountCalculator.calculate(secondBudget));
   }
 }

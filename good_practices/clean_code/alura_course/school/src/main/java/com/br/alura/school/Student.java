201d6e374361d8e3d3c310245f6861bd518bdd2e
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-10-23 10:04:17 -0300

    Add phones array to Student class

--- good_practices/clean_code/alura_course/school/src/main/java/com/br/alura/school/Student.java
+++ good_practices/clean_code/alura_course/school/src/main/java/com/br/alura/school/Student.java
@@ -1,9 +1,17 @@
 package com.br.alura.school;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
     
   private String name;
   private CPF cpf;
   private Email email;
   
+  private List<Phone> phones = new ArrayList<Phone>();
+
+  public void addPhone(String ddd, String number){
+    this.phones.add(new Phone(ddd, number));
+  }
 }

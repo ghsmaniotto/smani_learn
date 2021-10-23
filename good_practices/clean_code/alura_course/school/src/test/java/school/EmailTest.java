957d21dd05227bdd4dd302bd8c8ec4fc99e844ab
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-10-23 09:19:18 -0300

    Add test for email class

---
+++ good_practices/clean_code/alura_course/school/src/test/java/school/EmailTest.java
@@ -0,0 +1,28 @@
+package school;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.br.alura.school.Email;
+
+import org.junit.jupiter.api.Test;
+
+class EmailTest {
+
+  @Test
+  void mustNotCreateEmailWithInvalidAddress() {
+    assertThrows(IllegalArgumentException.class, () -> new Email(null));
+
+    assertThrows(IllegalArgumentException.class, () -> new Email(""));
+
+    assertThrows(IllegalArgumentException.class, () -> new Email("invalid_email"));
+  }
+
+  @Test
+  void mustCreateEmailWithValidAddress() {
+    final String validEmail = "valid@email.com";
+
+    assertDoesNotThrow(() -> new Email(validEmail));
+    
+    assertEquals(new Email(validEmail).getAddreess(), validEmail);
+  }
+}

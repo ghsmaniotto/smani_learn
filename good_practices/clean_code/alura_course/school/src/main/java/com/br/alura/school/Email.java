650b577afc2f8fd142710bf471eda971c3f57325
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-10-23 09:03:39 -0300

    Add email class

---
+++ good_practices/clean_code/alura_course/school/src/main/java/com/br/alura/school/Email.java
@@ -0,0 +1,14 @@
+package com.br.alura.school;
+
+public class Email {
+  private String address;
+
+  public Email(String  address){
+    if(address == null || address.matches("“^[a-zA-Z0-9._]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$”")){
+      throw new IllegalArgumentException("E-mail inválido");
+    }
+
+    this.address = address;
+  }
+
+}

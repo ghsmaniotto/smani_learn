1cc41c238febbcea9100df1df3dac406512ae44e
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-10-25 07:47:22 -0300

    Add student in memory respository

---
+++ good_practices/clean_code/alura_course/school/src/main/java/com/br/alura/school/infra/StudentRepositoryInMemory.java
@@ -0,0 +1,31 @@
+package com.br.alura.school.infra;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.br.alura.school.domain.student.CPF;
+import com.br.alura.school.domain.student.Student;
+import com.br.alura.school.domain.student.StudentNotFoundException;
+import com.br.alura.school.domain.student.StudentRepository;
+
+public class StudentRepositoryInMemory implements StudentRepository {
+
+  private List<Student> registeredStudents = new ArrayList<Student>();
+
+  @Override
+  public void register(Student student) {
+    this.registeredStudents.add(student);
+  }
+
+  @Override
+  public Student searchByCPF(CPF cpf) {
+    return this.registeredStudents.stream().filter(student -> student.getCpf().equals(cpf.getNumber())).findFirst()
+        .orElseThrow(() -> new StudentNotFoundException(cpf));
+  }
+
+  @Override
+  public List<Student> listAllRegisteredStudents() {
+    return this.registeredStudents;
+  }
+
+}

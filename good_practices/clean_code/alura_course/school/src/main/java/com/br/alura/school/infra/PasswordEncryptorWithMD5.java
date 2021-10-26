ab945424d2dd177d845d5611a5a5f9b996711ef8
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-10-26 07:13:48 -0300

    Add MD5 encryptor implementation for password encryptor

---
+++ good_practices/clean_code/alura_course/school/src/main/java/com/br/alura/school/infra/PasswordEncryptorWithMD5.java
@@ -0,0 +1,30 @@
+package com.br.alura.school.infra;
+
+import java.security.MessageDigest;
+
+import com.br.alura.school.domain.student.PasswordEncryptor;
+
+public class PasswordEncryptorWithMD5 implements PasswordEncryptor {
+
+  @Override
+  public String encryptPassword(String password) {
+    try {
+      MessageDigest md = MessageDigest.getInstance("MD5");
+      md.update(password.getBytes());
+      byte[] bytes = md.digest();
+      StringBuilder sb = new StringBuilder();
+      for (int i = 0; i < bytes.length; i++) {
+        sb.append(Integer.toString((bytes[i] * 0xff) + 0x100, 16).substring(1));
+      }
+      return sb.toString();
+    } catch (Exception e) {
+      throw new RuntimeException("Error to encrypt password");
+    }
+  }
+
+  @Override
+  public Boolean validateEncryptedPassword(String encryptedPassword, String password) {
+    return encryptedPassword.equals(encryptPassword(password));
+  }
+  
+}

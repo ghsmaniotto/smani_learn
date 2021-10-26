f5db0867cdf8d93e5119795409fb2a17aca6a010
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-10-26 07:41:41 -0300

    Add implementation for send email recommendation interface

---
+++ good_practices/clean_code/alura_course/school/src/main/java/com/br/alura/school/infra/recommendation/SendRecommendationEmailWithJavaMail.java
@@ -0,0 +1,13 @@
+package com.br.alura.school.infra.recommendation;
+
+import com.br.alura.school.application.recommendation.SendEmailRecommendation;
+import com.br.alura.school.domain.student.Student;
+
+public class SendRecommendationEmailWithJavaMail implements SendEmailRecommendation {
+
+  @Override
+  public void sentTo(Student student) {
+    // Send email using Java Mail package
+  }
+  
+}

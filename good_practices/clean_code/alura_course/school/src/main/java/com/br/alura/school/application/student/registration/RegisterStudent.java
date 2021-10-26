0ae3caac411d89d170de7895609e041e54145531
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-10-26 07:55:35 -0300

    Add register student service for the application

---
+++ good_practices/clean_code/alura_course/school/src/main/java/com/br/alura/school/application/student/registration/RegisterStudent.java
@@ -0,0 +1,20 @@
+package com.br.alura.school.application.student.registration;
+
+import com.br.alura.school.domain.student.Student;
+import com.br.alura.school.domain.student.StudentRepository;
+
+public class RegisterStudent {
+
+  private final StudentRepository repository;
+
+  public RegisterStudent(StudentRepository repository){
+    this.repository = repository;
+  }
+
+  public void register(RegisterStudentDTO data){
+    Student newStudent = data.createStudent();
+
+    this.repository.register(newStudent);
+  }
+  
+}

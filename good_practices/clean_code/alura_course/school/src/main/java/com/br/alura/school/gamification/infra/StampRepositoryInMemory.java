64ad8391256fb15d866b331bcd05d41fb3cf9799
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-11-26 08:23:18 -0300

    Add stamp repository in memory implementation

---
+++ good_practices/clean_code/alura_course/school/src/main/java/com/br/alura/school/gamification/infra/StampRepositoryInMemory.java
@@ -0,0 +1,27 @@
+package com.br.alura.school.gamification.infra;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.br.alura.school.academic.domain.student.CPF;
+import com.br.alura.school.gamification.domain.Stamp;
+import com.br.alura.school.gamification.domain.StampRepository;
+
+public class StampRepositoryInMemory implements StampRepository {
+
+  private List<Stamp> stamps = new ArrayList<Stamp>();
+
+  @Override
+  public List<Stamp> listStudentStamps(CPF cpf) {
+    return this.stamps.stream().filter(
+      stamp -> stamp.studentCpf().getNumber().equals(cpf.getNumber())
+      ).collect(Collectors.toList());
+  }
+
+  @Override
+  public void register(Stamp stamp) {
+    this.stamps.add(stamp);
+  }
+  
+}

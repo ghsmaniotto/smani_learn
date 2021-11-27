b9569ad22509b9e6a62a4674453db56202c3cece
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-11-27 11:35:08 -0300

    Add listener to craete new student stamp

---
+++ good_practices/clean_code/alura_course/school/src/main/java/com/br/alura/school/gamification/application/CrateNewStudentStamp.java
@@ -0,0 +1,31 @@
+package com.br.alura.school.gamification.application;
+
+import com.br.alura.school.gamification.domain.stamp.Stamp;
+import com.br.alura.school.gamification.domain.stamp.StampRepository;
+import com.br.alura.school.shared.domain.CPF;
+import com.br.alura.school.shared.domain.event.Event;
+import com.br.alura.school.shared.domain.event.EventType;
+import com.br.alura.school.shared.domain.event.Listener;
+
+public class CrateNewStudentStamp extends Listener {
+
+  private final StampRepository repository;
+
+  public CrateNewStudentStamp(StampRepository repository) {
+    this.repository = repository;
+  }
+
+  @Override
+  protected boolean mustProcess(Event event) {
+    return event.type() == EventType.REGISTERD_STUDENT;
+  }
+
+  @Override
+  protected void reactTo(Event event) {
+    CPF studentCpf = (CPF) event.data().get("cpf");
+    Stamp newStudent = new Stamp(studentCpf, "New Student");
+
+    this.repository.store(newStudent);
+  }
+
+}

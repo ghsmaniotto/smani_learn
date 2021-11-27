0d31b188268c48fe27d19c1959b9331b4f85acd5
Smaniotto <ghsmaniotto@inf.ufpel.edu.br>
2021-11-27 11:23:52 -0300

    Add data method to event interface

--- good_practices/clean_code/alura_course/school/src/main/java/com/br/alura/school/shared/domain/event/Event.java
+++ good_practices/clean_code/alura_course/school/src/main/java/com/br/alura/school/shared/domain/event/Event.java
@@ -1,10 +1,12 @@
 package com.br.alura.school.shared.domain.event;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 public interface Event {
 
   LocalDateTime moment();
   EventType type();
+  Map<String, Object> data();
 
 }
